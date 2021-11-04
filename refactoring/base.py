b333299cb6d1a41a756a0061d3a387a22e87598b
Jin <haya.jinlee@gmail.com>
2021-11-04 10:51:23 +0100

    Add base classes for single/multi sessions and demo notebook

--- refactoring/base.py
+++ refactoring/base.py
@@ -8,7 +8,7 @@ import pandas as pd
 ## Crawling all data saved in Data folder
 
 
-def read_data(root_path, tag):
+def read_data(root_path, tag, has_beacon=True):
     """
     root_path: root directory where data is ex) ../Data/Raw/
     tag: date_time_tag to search for beacon/position data ex) '20211028-0030984'
@@ -26,9 +26,9 @@ def read_data(root_path, tag):
     for p in position_paths:
         if tag in p:
             position_data = pd.read_csv(p, sep=" ", header=None)
-    if beacon_data is None or position_data is None:
-        ValueError(f'One of beacon or position data is missing in {tag}')
-    return beacon_data, position_data
+    if has_beacon and beacon_data is None or position_data is None:
+        raise ValueError(f'One of beacon or position data is missing in {tag}')
+    return position_data, beacon_data
 
 
 def multiple_days_data(root_path, tags):
@@ -52,22 +52,101 @@ def make_trials(beacon_data, position_data):
     return None
 
 
+def rotation_correction(position_data):
+
+    alpha = (5) * np.pi / 180
+
+    rot_position_data = position_data
+
+    rot_position_data[:, 1] = position_data[:, 1] * np.cos(
+        alpha) - position_data[:, 3] * np.sin(alpha)
+
+    rot_position_data[:, 3] = position_data[:, 1] * np.sin(
+        alpha) + position_data[:, 3] * np.cos(alpha)
+
+    return rot_position_data
+
+
 class BeaconPosition():
-    def __init__(self, root_path, tag):
-        self.beacon_data, self.posisiton_data = read_data(root_path, tag)
-        self.beacon_data = beacon_data
-        self.position_data = position_data
-        self.trials_data = make_trials(beacon_data, position_data)
+    def __init__(self, root_path, tag, has_beacon=True):
+        self.position_data, self.beacon_data = read_data(
+            root_path, tag, has_beacon)
+        self.position_data = rotation_correction(self.position_data.to_numpy())
+        if has_beacon:
+            self.beacon_data = self.beacon_data.to_numpy()
+            self.trials_data = make_trials(self.beacon_data,
+                                           self.position_data)
+
+        self.get_distance_speed()
+        self.statistics = self.get_statistic()
 
     def get_statistic(self):
-        """
-        Implement method to get a statistic for one day data
-        
-        """
-        pass
+
+        self.avg_speed = np.mean(self.speed)
+        self.median_speed = np.median(self.speed)
+        self.total_distance = self.travel_distance[-1]
+
+        return {
+            'avg_speed': self.avg_speed,
+            'median_speed': self.median_speed,
+            'distance': self.total_distance
+        }
+
+    def get_distance_speed(self):
+
+        self.displacement = np.sqrt(
+            np.sum((self.position_data[1:, 1:3] -
+                    self.position_data[:-1, 1:3])**2,
+                   axis=1))
+        self.travel_distance = np.cumsum(self.displacement, axis=0)
+        self.time_bin = self.position_data[1:, 0] - self.position_data[:-1, 0]
+        self.speed = (self.displacement / self.time_bin) * 100  #Check unit
 
 
 class MultiDaysBeaconPosition():
-    def __init__(self, root_path, tags):
-        self.beacon_list, self.position_list = multiple_days_data(
-            root_path, tags)
+    def __init__(self, root_path, tags, has_beacon):
+        #self.beacon_list, self.position_list = multiple_days_data(
+        #    root_path, tags)
+        self.dataset_list = []
+        for tag in tags:
+            self.dataset_list.append(BeaconPosition(root_path, tag,
+                                                    has_beacon))
+        self.multisession_statistics, self.individual_statistics = self.get_statistics(
+        )
+
+    @property
+    def num_sessions(self):
+        return len(self.dataset_list)
+
+    def get_statistics(self):
+        ## Across session statistics
+        self.avg_speed = np.mean(
+            [session.avg_speed for session in self.dataset_list])
+        self.avg_distance = np.mean(
+            [session.total_distance for session in self.dataset_list])
+        self.median_speed = np.median(
+            np.concatenate([session.speed for session in self.dataset_list]))
+
+        multisession_statistics = {
+            'avg_speed': self.avg_speed,
+            'avg_distance': self.avg_distance,
+            'median_speed': self.median_speed
+        }
+
+        ##Individual session statistics
+        self.avg_speed_per_session = [
+            session.avg_speed for session in self.dataset_list
+        ]
+        self.median_speed_per_session = [
+            session.median_speed for session in self.dataset_list
+        ]
+        self.distance_per_session = [
+            session.total_distance for session in self.dataset_list
+        ]
+        individual_statistics = {
+            'avg_speeds': self.avg_speed_per_session,
+            'distances': self.distance_per_session,
+            'median_speeds': self.median_speed_per_session
+        }
+
+        return multisession_statistics, individual_statistics
