9705b930f23ce900e8f6596982c76073cf63e1b3
Jin <haya.jinlee@gmail.com>
2021-11-22 00:12:32 +0900

    Include visibility in data base

--- refactoring/base.py
+++ refactoring/base.py
@@ -5,7 +5,7 @@ import argparse
 import numpy as np
 import pandas as pd
 
-## Crawling all data saved in Data folder
+## Filtering of speed, position, **sampling rate differs across trials!
 
 
 def read_data(root_path, tag, has_beacon=True, has_metadata=True):
@@ -30,7 +30,7 @@ def read_data(root_path, tag, has_beacon=True, has_metadata=True):
             position_data = pd.read_csv(p, sep=" ", header=None)
     for p in meta_paths:
         if tag in p:
-            metadata = pd.read_csv(p, sep=":", header=None)
+            metadata = pd.read_csv(p, sep=" : ", header=None, index_col=0).T
     if has_beacon and beacon_data is None or position_data is None:
         raise ValueError(f'One of beacon or position data is missing in {tag}')
     if has_metadata and metadata is None:
@@ -53,8 +53,17 @@ def multiple_days_data(root_path, tags):
 
 
 def make_trials(position_data, beacon_data, metadata):
+    '''
+    Return
+    trial_list: list of numpy arrays position data [time, x, y, z] 
+    trial_beacon: list of beacon position
+    trial_visible: list of booleans referring visibility of trial (True - visible, False- invisible
+    before_beacon: list of positions before the first beacon appears
+    '''
+
     trial_list = []
     trial_beacon = beacon_data[:, -2:]
+    sampling_rate = position_data[1, 0] - position_data[0, 0]
     beacon_shown_time_idx = [
         np.argmin(abs(position_data[:, 0] - i)) for i in beacon_data[:, 0]
     ]
@@ -66,8 +75,22 @@ def make_trials(position_data, beacon_data, metadata):
             trial = position_data[beacon_shown_time_idx[i]:]
         trial_list.append(trial)
     before_beacon = position_data[:beacon_shown_time_idx[0]]
+    visible = []
+    trial_visible = [True] * len(trial_list)
+    invisible_time = eval(metadata['invisible_time'].item())
+    invisible_index = eval(metadata['invisible_list'].item())
 
-    return trial_list, trial_beacon, before_beacon
+    for n, trial in enumerate(trial_list):
+        trial_visible_ = np.ones_like(trial)
+        if n in invisible_index:
+            time_after = np.cumsum(trial[1:, 0] - trial[:-1, 0])
+            if time_after[-1] > invisible_time:
+                invisible_end = np.where(time_after >= invisible_time)[0][0]
+                trial_visible_ = trial_visible_[:invisible_end]
+            trial_visible[n] = False
+        visible.append(trial_visible_)
+
+    return trial_list, trial_beacon, visible, trial_visible
 
 
 def rotation_correction(position_data):
@@ -86,17 +109,22 @@ def rotation_correction(position_data):
 
 
 class BeaconPosition():
+    """
+    Single session data class
+    ##TODO: extend trial_visible into time base, not trial base
+    """
     def __init__(self, root_path, tag, has_beacon=True, has_metadata=True):
         self.position_data, self.beacon_data, self.metadata = read_data(
             root_path, tag, has_beacon, has_metadata)
-        self.position_data = rotation_correction(self.position_data.to_numpy())
+        self.position_data = rotation_correction(
+            self.position_data.to_numpy()[:, :4])
         if has_beacon:
             self.beacon_data = self.beacon_data.to_numpy()
 
         self.get_distance_speed()
         self.statistics = self.get_statistic()
         if has_beacon:
-            self.trial_list, self.trial_beacon, self.before_beacon = make_trials(
+            self.trial_list, self.trial_beacon, self.visible, self.trial_visible = make_trials(
                 self.position_data, self.beacon_data, self.metadata)
 
     def get_statistic(self):
@@ -142,11 +170,13 @@ class MultiDaysBeaconPosition():
     def get_trials(self):
         self.trial_list = []
         self.beacon_list = []
-        self.before_beacon = []
+        self.trial_visible = []
+        self.visible = []
         for session in self.dataset_list:
             self.trial_list.append(session.trial_list)
+            self.trial_visible.append(session.trial_visible)
+            self.visible.append(session.visible)
             self.beacon_list.append(session.trial_beacon)
-            self.before_beacon.append(session.before_beacon)
 
     def get_statistics(self):
         ## Across session statistics
--- refactoring/path_analysis.py
+++ refactoring/path_analysis.py
@@ -2,8 +2,42 @@ import numpy as np
 
 import base
 
+##TODOs: implement ver.1 straightness function, bootstraping
 
-def straightness_over_time(trial_trajectory, from_start=200):
+
+def straightness_moment_time(trial_trajectory, before_time=3):
+    def _straight_line(start, end, length):
+
+        _x = np.linspace(start[0], end[0], length)
+        _y = np.linspace(start[1], end[1], length)
+
+        return np.vstack([_x, _y]).T
+
+    def _straight_length(start, end):
+
+        return np.sqrt(np.sum((start - end)**2))
+
+    def travel_distance(traj):
+        return np.cumsum(np.sqrt(np.sum((traj[1:] - traj[:-1])**2,
+                                        axis=1)))[-1]
+
+    time = trial_trajectory[:, 0]
+    time_after = np.cumsum(np.flip(time[1:] - time[:-1]))
+    if time_after[-1] > before_time:
+        end_ind = np.where(time_after >= before_time)[0][0]
+        trial_trajectory = np.flip(trial_trajectory[:, 1:3, ])[end_ind:]
+
+    straight_length = _straight_length(trial_trajectory[0],
+                                       trial_trajectory[-1])
+    trajectory_displacement = travel_distance(trial_trajectory)
+    straightness = straight_length / trajectory_displacement
+
+    return straightness, np.array([trial_trajectory[0],
+                                   trial_trajectory[-1]]), trial_trajectory
+
+
+def straightness_over_time(trial_trajectory, before_time=2):
+
     straightness = []
 
     def _straight_line(start, end, length):
@@ -21,13 +55,17 @@ def straightness_over_time(trial_trajectory, from_start=200):
         return np.cumsum(np.sqrt(np.sum((traj[1:] - traj[:-1])**2,
                                         axis=1)))[-1]
 
-    trial_trajectory = trial_trajectory[-from_start:]
-
-    for i in range(1, from_start - 1):
+    time = trial_trajectory[:, 0]
+    time_after = np.cumsum(np.flip(time[1:] - time[:-1]))
+    if time_after[-1] > before_time:
+        end_ind = np.where(time_after >= before_time)[0][0]
+        trial_trajectory = np.flip(trial_trajectory[:, 1:3, ])[end_ind:]
+    for i in range(len(trial_trajectory) - 1):
         straight_length = _straight_length(trial_trajectory[i],
                                            trial_trajectory[-1])
         trajectory_displacement = travel_distance(trial_trajectory[i:])
 
         straightness.append(straight_length / trajectory_displacement)
 
-    return straightness
+    return straightness, (trial_trajectory[0],
+                          trial_trajectory[-1]), trial_trajectory
